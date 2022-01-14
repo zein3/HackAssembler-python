7f226a717231467c6355102b6507afd0cc803f65
zein3 <zeinhaddad02@gmail.com>
2022-01-14 08:27:32 +0700

    removed redundancies

--- src/hack_assembler/assembler/__init__.py
+++ src/hack_assembler/assembler/__init__.py
@@ -1,3 +1,11 @@
+# If instruction starts with @n where n is an integer, it's an A-instruction,
+# the resulting machine code will be: '0' + binary(n).
+# otherwise it is a C-instruction, therefore it must be parsed into three components:
+# - comp
+# - dest
+# - jump
+# and the resulting machine code will be '111' + comp + dest + jump
+
 from . import a_translator
 from . import c_translator
 
